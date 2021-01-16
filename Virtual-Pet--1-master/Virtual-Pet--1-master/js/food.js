a18f25c8ac70fd568f331ff05667a5248b4727e7
Astrogamer2020 <70177647+Astrogamer2020@users.noreply.github.com>
2021-01-16 11:25:52 -0500

    Add files via upload

---
+++ Virtual-Pet--1-master/Virtual-Pet--1-master/js/food.js
@@ -0,0 +1,47 @@
+class Foodclass{
+constructor(foodStock,lastFed){
+
+    this.image = loadImage("images/Milk.png");
+}
+
+    
+       
+    
+
+    getFoodStock(){
+
+
+    }
+
+    updateFoodStock(){
+
+
+    }
+
+    deductFood(){
+
+
+    }
+
+
+    display(){
+var x=80 ,y=100;
+imageMode(CENTER);
+
+
+if(this.foodStock!=0){
+
+}
+for(var i=0;i<this.foodStock;i++){
+if(1%10==0){
+x=80
+y=y+50
+}
+
+
+if(mousePressed)
+x=x+30;
+}
+    }
+}
+
